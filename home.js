3c3157ce471e006b5f096fe9d54a6706db3d0a55
Paloma1104 <palomajain11@gmail.com>
2024-10-20 00:47:46 +0530

    api update

--- home.js
+++ home.js
@@ -1,19 +1,9 @@
-var btc = document.getElementById("bitcoin");
-var eth = document.getElementById("ethereum");
-var doge = document.getElementById("dog");
+const options = {
+  method: 'GET',
+  headers: {accept: 'application/json', 'x-cg-pro-api-key': 'CG-B9g6UX1cwQWzY2SD7L9Lp6UJ	'}
+};
 
-var url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd";
-
-fetch(url)
+fetch('https://pro-api.coingecko.com/api/v3/simple/price?ids=bitcoin%2C%20dogecoin%2C%20ethereum&vs_currencies=usd&include_24hr_change=true', options)
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    
-    // Assuming you want to update the innerText of the elements with prices
-    btc.innerText = Bitcoin: $${data.bitcoin.usd};
-    eth.innerText = Ethereum: $${data.ethereum.usd};
-    doge.innerText = Dogecoin: $${data.dogecoin.usd};
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
